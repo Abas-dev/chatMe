e8fc37ad37e7d187d6b34ef191ca7cffbbebe4a0
abas-dev <abasisaac22@gmail.com>
2023-06-15 21:42:04 +0100

    added templates and views

--- LetsChat/settings.py
+++ LetsChat/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # created apps 
+    'chatApp.apps.ChatappConfig',
+    #third party apps 
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'LetsChat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- chatApp/views.py
+++ chatApp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
 
-# Create your views here.
+class ChatPage(TemplateView):
+    template_name = 'base.html'
+    
+class LoginPage(LoginView):
+    pass
+
+class RegisterPage(CreateView):
+    pass
